79f6630ba05c0a4902ec2980b0e478d32f4378c1
Luc Gaitskell <me@lucg.xyz>
2023-07-09 01:03:14 -0700

    Use floats instead of int

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ from lights.animations.strobe import Strobe
 
 hz = 10
 
-anim = Strobe(pixels, hz, (0, 0, 0, 255))
+anim = Strobe(pixels, hz, (0, 0, 0, 1.0))
 print("Running at {}Hz".format(hz))
 while True:
     anim.animate()
